d268aedb0056739b31a382b8d6c68b4873ef71bb
Viktor Kirillov <mr.kirillov@icloud.com>
2019-07-14 13:37:59 -0500

    Added reducer to Pipeline(now able to process statistics of data)

--- customPipeline.py
+++ customPipeline.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[22]:
+# In[63]:
 
 
 from tqdm import tqdm
+import time
 
 # arr = [x for x in range(1000000)]
 
@@ -14,19 +15,45 @@ class Pipe:
     
     # Applies all the functions in pipeline to each element. Returns modified array
     def __call__(self, data):
-        for func in self.pipes:
-            data = list(map(func, tqdm(data, desc=func.__name__)))
+        for (func, pipeType) in self.pipes:
+            if pipeType == 'i':
+                data = list(map(func, tqdm(data, desc=func.__name__)))
+            elif pipeType == 'd':
+                data = list(filter(func, tqdm(data, desc=func.__name__)))
+            elif pipeType == 's':
+                (reducer,printer,initializer) = func
+                self.__reduce(reducer, printer, tqdm(data, desc=reducer.__name__), initializer)
+                time.sleep(2) # To display stats normally
         return data
     
     # Adds function to Pipe.process pipeline
-    def addPipe(self, func):
-        # Checking for type of argument
-        if type(func) is list:
-            # arg func is <class 'list'>
-            self.pipes += func
-        elif hasattr(func, '__call__'):
+    def addItemPipe(self, func):
+        if hasattr(func, '__call__'):
             # arg func is <class 'function'>
-            self.pipes.append(func)
+            self.pipes.append((func, 'i'))
+            
+    def addDataPipe(self, func):
+        if hasattr(func, '__call__'):
+            # arg func is <class 'function'>
+            self.pipes.append((func, 'd'))
+            
+    def addStatPipe(self, reducer, printer, initializer):
+        if hasattr(reducer, '__call__') and hasattr(printer, '__call__'):
+            # arg func is <class 'function'>
+            self.pipes.append(((reducer,printer,initializer),'s'))
+            
+    def __reduce(self, reducer, printer, iterable, initializer=None):
+        it = iter(iterable)
+        if initializer is None:
+            try:
+                initializer = next(it)
+            except StopIteration:
+                raise TypeError('reduce() of empty sequence with no initial value')
+        accum_value = initializer
+        for x in it:
+            accum_value = reducer(accum_value, x)
+        # Last iteration
+        printer(accum_value)
         
 # def addOne(item):
 #     return item + 1
@@ -34,14 +61,29 @@ class Pipe:
 # def square(item):
 #     return item ** 2
 
-# def printItem(item):
-#     print(item)
-#     return item
+# def onlyOdd(item_original):
+#     if item_original % 2 == 1:
+#         return True
+#     return False
+
+# def countStats(acc, item):
+#     acc["total"] += 1
+#     return acc
+# def printStats(stats):
+#     print(stats["total"], "items were processed in total.")
 
 # pipe = Pipe()
-# pipe.addPipe([addOne])
-# pipe.addPipe([square])
+# pipe.addItemPipe(addOne)
+# pipe.addDataPipe(onlyOdd)
+# pipe.addStatPipe(countStats, printStats, {"total":0})
+# pipe.addItemPipe(square)
 # res = pipe(arr)
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
